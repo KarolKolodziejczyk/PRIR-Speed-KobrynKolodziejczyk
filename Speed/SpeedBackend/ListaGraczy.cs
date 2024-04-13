b6738a91c848647b9ea983c0fce7cfe54de93d05
KarolKolodziejczyk <kk303201@student.polsl.pl>
2024-04-13 23:17:52 +0200

    Dodanie Scannera

--- Speed/SpeedBackend/ListaGraczy.cs
+++ Speed/SpeedBackend/ListaGraczy.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-
+//Tutaj biorę z swojego starego projektu funkcje jak coś
 namespace SpeedBackend
 {
     internal class ListaGraczy : IPlansza
@@ -11,8 +14,69 @@ namespace SpeedBackend
         List<String> ListaIp = new List<string>();
 
 
-        void SearchForPlayers() { 
-            
+        void SearchForPlayers() {
+            NetworkInterface[] interfejsy = NetworkInterface.GetAllNetworkInterfaces();
+            Parallel.ForEach(interfejsy, networkInterface =>
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties interfaceProperties = networkInterface.GetIPProperties();
+                    foreach (UnicastIPAddressInformation ipAddressInfo in interfaceProperties.UnicastAddresses)
+                    {
+                        if (ipAddressInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            IPAddress subnetMask = ipAddressInfo.IPv4Mask;
+                            IPAddress ipAddress = ipAddressInfo.Address;
+
+                            if (!IPAddress.IsLoopback(ipAddress))
+                            {
+                                IEnumerable<string> pingableIps = GetPingableIPs(ipAddress, subnetMask);
+
+                                lock (ListaIp)
+                                {
+                                    ListaIp.AddRange(pingableIps);
+                                }
+
+                            }
+                        }
+                    }
+                }
+            });
+
+
+        }
+        private static IEnumerable<string> GetPingableIPs(IPAddress ipAddress, IPAddress subnetMask)
+        {
+            List<string> pingableIPs = new List<string>();
+
+            byte[] ipBytes = ipAddress.GetAddressBytes();
+            byte[] subnetBytes = subnetMask.GetAddressBytes();
+
+            for (int i = 0; i < ipBytes.Length; i++)
+            {
+                ipBytes[i] &= subnetBytes[i];
+            }
+
+            IPAddress networkAddress = new IPAddress(ipBytes);
+
+            for (int i = 1; i < 255; i++)
+            {
+                ipBytes[ipBytes.Length - 1] = (byte)i;
+                IPAddress pingIP = new IPAddress(ipBytes);
+
+                Ping ping = new Ping();
+                PingReply reply = ping.Send(pingIP, 3);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    pingableIPs.Add(pingIP.ToString());
+                }
+            }
+
+            return pingableIPs;
         }
     }
+ 
+
 }
+
